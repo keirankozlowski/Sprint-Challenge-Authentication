8b611927389bd83158a59b1b9a85cab60ff00648
Keiran <keirankozlowski@gmail.com>
2018-10-19 11:23:59 -0400

    Set up imports in server.js

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const knex = require('knex');
+const knexConfig = require('./knexfile.js');
+
+const db = knex(knexConfig.development);
+const server = express();
+
+server.use(express.json());
+server.use(cors());
 
 const configureRoutes = require('./config/routes');
+configureRoutes(server);
 
-const server = express();
 const corsOptions = {
   // If you're moving onto the stretch problem you'll need to set this obj with the appropriate fields
   // ensure that your client's URL/Port can achieve a Handshake
   // then pass this object to the cors() function
 };
-
-server.use(express.json());
-server.use(cors());
-
-configureRoutes(server);
 
 module.exports = {
   server,
